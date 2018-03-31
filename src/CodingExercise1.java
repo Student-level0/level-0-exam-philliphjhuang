d936eacdb3f7083052dac89d43357ccd105c7d43
philliphjhuang <League@AdministorsiMac.attlocal.net>
2018-03-30 18:24:26 -0700

    .
    .

--- src/CodingExercise1.java
+++ src/CodingExercise1.java
@@ -5,15 +5,35 @@ import org.jointheleague.graphical.robot.Robot;
 /* Level 0 Exam: Coding  Exercise #1 */
 public class CodingExercise1 {
 
-    public static void main(String[] args) {
-   	 // 3. ask the user what color they would like the Robot to draw
+	public static void main(String[] args) {
+		Robot rob = new Robot();
+		rob.setSpeed(10);
 
-   	 // 4. use an if/else statement to set the pen color that the user requested (minimum of 2 colors)
+		// 3. ask the user what color they would like the Robot to draw
 
-   	 // 2. set the pen width to 10
+		// 4. use an if/else statement to set the pen color that the user requested
+		// (minimum of 2 colors)
 
-   	 // 1. make the Robot draw a shape
-   	 
-    }
+		// 2. set the pen width to 10
+		rob.penDown();
+		rob.setPenWidth(10);
+		String x = JOptionPane.showInputDialog("What color would you like the robot to draw?");
+		// 1. make the Robot draw a shape
+		if (x.equalsIgnoreCase("red")) {
+			rob.setPenColor(255, 0, 0);
+			;
+
+		} else if (x.equalsIgnoreCase("Blue")) {
+			rob.setPenColor(0, 0, 255);
+
+		} else {}
+
+		for (int i = 0; i <= 3; i++) {
+			rob.move(50);
+			rob.turn(90);
+
+		}
+		rob.hide();
+	}
 
 }
--- src/CodingExercise2.java
+++ src/CodingExercise2.java
@@ -1,17 +1,20 @@
 
-
 /* Level 0 Exam: Coding  Exercise #2 */
 import javax.swing.JOptionPane;
 
 /**
- * Write a program that asks the user for their age. 
- * Tell them which year they were born. 
- * If they are over 30, tell them they are too old to play this game.
+ * Write a program that asks the user for their age. Tell them which year they
+ * were born. If they are over 30, tell them they are too old to play this game.
  **/
 
 public class CodingExercise2 {
-
+	public static void main(String[] args) {
+		String age = JOptionPane.showInputDialog("What is your age?");
+		int x = Integer.parseInt(age);
+		if (x > 30) {
+			JOptionPane.showMessageDialog(null, "You are too old to play this game.");
+		} else {
+			JOptionPane.showMessageDialog(null, "You are born on " + (2018 - x));
+		}
+	}
 }
-
-
-
